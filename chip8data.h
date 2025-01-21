076e56f4106800b28565f64c222a6118296b79da
Mourad Sherif <sherm5344@gmail.com>
2025-01-21 18:06:57 -0500

    Constructor fixed. Fonts included. PRNG function added.

--- chip8data.h
+++ chip8data.h
@@ -1,12 +1,15 @@
 #ifndef CHIP8DATA
 #define CHIP8DATA
 
+#include <random>  // for random number generation
+#include <chrono>  // for system clock, to seed PRNG
 #include <cstdint> // for fixed width ints below
 #include <fstream> // for file IO with ROM files
 
 class Chip8
 {
 public:
+    Chip8();                    // Declare the constructor IN the constructor so we can modify its parameters as we wish in the .cpp file
     uint8_t registers[16]{};    // 16 8-bit registers on the chip8
     uint8_t memory[4096]{};     // 4KB of memory
     uint16_t index{};           // 16-bit Index Register, store memory address in use for operations, 16-bit to hold max 0xFFF (4095) memory address
@@ -18,6 +21,9 @@ public:
     uint8_t keypad[16]{};       // 16 input keys, mapped as 1234, QWER, ASDF, ZXCV for this implementation
     uint32_t video[64 * 32]{};  // 64*32 pixel monitor, every pixel runs off a XOR implementation. A sprite that is loaded to a pixel will be shown as on, but anything overlapping it will be turned off. See XOR truth tables online.
     uint16_t opcode;            // Opcode, will be used to instruct what operation to perform, here the Chip8 will take the opcode and do whatever it is instructed to do
+
+   	std::default_random_engine randGen;              // random number engine that produces pseudo-random numbers
+	std::uniform_int_distribution<uint8_t> randByte; // return uniformly distributed random integers over a given interval, returns unsigned 8-bit int
 
     void LoadROM(char const* filename); // load ROM file instructions into memory, starting at 0x200
 };
--- chip8main.cpp
+++ chip8main.cpp
@@ -1,6 +1,30 @@
 #include "chip8data.h"
 
-const unsigned int START_ADDRESS = 0x200; // start address for Chip8, since 0x00 to 0x1FF is reserved for the Chip8
+const unsigned int START_ADDRESS = 0x200;        // start address for Chip8, since 0x00 to 0x1FF is reserved for the Chip8
+const unsigned int FONTSET_SIZE = 80;            // 16 characters used in total, each character is 5 bytes, so we have 80 bytes to use for font storage
+const unsigned int FONTSET_START_ADDRESS = 0x50; // fontset is loaded from 0x50 onwards
+
+
+// each line is for a given character, where if the binary data was lined up, it'd look like the corresponding character! See FONTS.txt.
+uint8_t fontset[FONTSET_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 void Chip8::LoadROM(char const* filename)
 {
@@ -30,7 +54,20 @@ void Chip8::LoadROM(char const* filename)
 }
 
 Chip8::Chip8()
+// Initialize random number engine with time since clock start
+: randGen(std::chrono::system_clock::now().time_since_epoch().count())
 {
 	// Initialize PC to 0x200
 	pc = START_ADDRESS;
+
+	// Load fonts into memory, starting at 0x50
+	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[FONTSET_START_ADDRESS + i] = fontset[i];
+	}
+
+	// Get random integer from distribution, 255U means UNSIGNED 255, constants/literals can have suffixes for this stuff
+	randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+
+
 }
